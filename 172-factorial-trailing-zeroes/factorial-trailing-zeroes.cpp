4d5642289effd385dc3268b459f5fb6c7121bcee
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-04-11 23:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.80%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,7 +1,11 @@
 class Solution {
-public: 
-   
+public:
     int trailingZeroes(int n) {
-         return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+        int cout = 0;
+         while(n>0){
+            n/=5;
+                           cout+=n;
+        }
+        return cout;
     }
 };
