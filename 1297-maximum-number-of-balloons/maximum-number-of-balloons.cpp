c226b757c99cf633a380c5fa2dc5b30e419b3eda
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-04-28 23:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.97%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int         maxNumberOfBalloons(string text) {
+                  int freq[5] = {0};
+        for (char c : text) {
+            if (c == 'b') freq[0]++;
+                     else if (c == 'a') freq[1]++;
+            else if (c == 'l') freq[2]++;
+               else if (c == 'o') freq[3]++;
+                   else if (c == 'n') freq[4]++;
+        }
+                             freq[2] /= 2;
+        freq[3] /= 2;
+        int res = freq[0];
+                     for (int i = 1; i < 5; ++i) {
+            res = min(res, freq[i]);
+        }
+        return res;
+    }
+};
