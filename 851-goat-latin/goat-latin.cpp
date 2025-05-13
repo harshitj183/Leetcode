4b73181de922acf5bea6aef557a334620cd6a8fe
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-05-13 22:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (82.64%) - LeetSync

---
+++ 851-goat-latin/goat-latin.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string toGoatLatin(string sentence) {
+          istringstream iss(sentence);
+         string word, result = "";
+        int index = 1;
+           string vowels = "aeiouAEIOU";
+
+                            while (iss >> word) {
+                        if (vowels.find(word[0]) != string::npos) {
+                word += "ma";
+            } 
+            else {
+                                 word = word.substr(1) + word[0] + "ma";
+            }
+
+                                  word += string(index, 'a');
+
+                            if (!result.empty()) result += " ";
+            result += word;
+            
+            index++;
+        }
+
+        return result;
+    }
+};
