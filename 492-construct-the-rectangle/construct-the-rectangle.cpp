4dc62ffe77d79aa8edbae107145e21040b1476db
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-05-15 22:31:13 +0530

    Time: 220 ms (5.42%) | Memory: 8 MB (53.80%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int bestL = area;
+               int bestW = 1;
+         int minDiff = area - 1;
+
+        for (int w = 1; w <= area; w++) {
+                    if (area % w == 0) {
+                int l = area / w;
+                if (l >= w && (l - w) < minDiff) {
+                           bestL = l;
+                    bestW = w;
+                     minDiff = l - w;
+                }
+            }
+        }
+
+        return {bestL, bestW};
+    }
+};
