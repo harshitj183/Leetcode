a1646eb1581bb6493b2ad17bb351b34f32d72529
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-05-20 23:33:53 +0530

    Time: 4 ms (49.10%) | Memory: 11.8 MB (75.98%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        for (char c : ransomNote) {
+                       size_t pos = magazine.find(c);
+            if (pos == string::npos) {
+                     return false;  
+                       } else {
+                magazine.erase(pos, 1);  
+                    }
+        }
+        return true;
+    }
+};
