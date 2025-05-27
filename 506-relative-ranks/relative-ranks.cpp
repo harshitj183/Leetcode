c8279c3f9daa45f47bcc3e381f6919e7adfd4e19
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-05-27 23:46:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (81.72%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,29 @@
+ class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<pair<int, int>> scoreWithIndex;
+
+        for (int i = 0; i < n; i++) {
+            scoreWithIndex.push_back({score[i], i});
+        }
+
+        sort(scoreWithIndex.begin(), scoreWithIndex.end(), greater<pair<int, int>>());
+
+        vector<string> result(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = scoreWithIndex[i].second;
+            if (i == 0)
+                result[index] = "Gold Medal";
+            else if (i == 1)
+                result[index] = "Silver Medal";
+            else if (i == 2)
+                result[index] = "Bronze Medal";
+            else
+                result[index] = to_string(i + 1);
+        }
+
+        return result;
+    }
+};
