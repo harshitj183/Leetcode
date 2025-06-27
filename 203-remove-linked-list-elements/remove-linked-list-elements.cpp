7f113e03090077ed4b4c5f8fedc05ba1e1fddf1a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-06-27 23:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (48.27%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        // Dummy node banaya to handle edge case when head node needs to be deleted
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        
+        ListNode* current = dummy;
+        
+        while (current->next != nullptr) {
+            if (current->next->val == val) {
+                // Skip the node with value equal to val
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp; // memory free karna important hai
+            } else {
+                current = current->next;
+            }
+        }
+        
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
