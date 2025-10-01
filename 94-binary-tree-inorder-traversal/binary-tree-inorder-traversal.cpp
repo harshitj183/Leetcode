6eb3b30689bd18482c10de336fe1340ddd81921a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-10-01 23:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (32.53%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+
+    void inorder(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+        inorder(node->left, res);
+        res.push_back(node->val);
+        inorder(node->right, res);
+    }
+};
