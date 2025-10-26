75420f69795050f52b56cb0902523d878782128a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-10-26 23:21:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (90.46%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -7,12 +7,21 @@ public:
          
         while (i >= 0 && s[i] == ' ') {
             i--;
+
         }
 
-                
+
+
+
+
+
+
+
+
                        while (i >= 0 && s[i] != ' ') {
             length++;
             i--;
+            ;
         }
 
               return length;
