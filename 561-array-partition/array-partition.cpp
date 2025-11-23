aae77d0a45e5e2eada39300704ab8e693be9dc1a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-11-23 23:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (81.40%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int arrayPairSum(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (size_t i = 0; i < nums.size(); i += 2)
+            sum += nums[i];
+        return sum;
+    }
+};
