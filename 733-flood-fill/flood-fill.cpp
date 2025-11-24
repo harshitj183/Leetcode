6e494c6616dbbfbc7f2bed45ebbc812ad15c8c59
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-11-24 23:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (55.88%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int r, int c, int orig, int color) {
+        int m = image.size(), n = image[0].size();
+
+        // Out of bounds
+        if (r < 0 || r >= m || c < 0 || c >= n)
+            return;
+
+        // Not the target color
+        if (image[r][c] != orig)
+            return;
+
+        // Fill current cell
+        image[r][c] = color;
+
+        // Explore 4-directionally
+        dfs(image, r + 1, c, orig, color);
+        dfs(image, r - 1, c, orig, color);
+        dfs(image, r, c + 1, orig, color);
+        dfs(image, r, c - 1, orig, color);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int orig = image[sr][sc];
+
+        // If original color is same as new color, no need to fill
+        if (orig == color)
+            return image;
+
+        dfs(image, sr, sc, orig, color);
+        return image;
+    }
+};
