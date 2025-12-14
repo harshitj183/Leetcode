534eede405197dff8c929423fafb0dff1300625f
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2025-12-14 22:50:48 +0530

    Time: 6 ms (59.14%) | Memory: 25.7 MB (73.12%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                int newIndex = (i + nums[i]) % n;
+                if (newIndex < 0)
+                    newIndex += n;
+
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+};
