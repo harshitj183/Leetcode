e89bfe8de6b7e745c72c5a7b72c3c8b5f7d3f12b
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-01-06 22:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (81.01%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+int n = nums.size();
+ sort(nums.begin(), nums.end());
+int i =0;
 
-        // Step 1 & 2: Flip negative numbers
-        for (int i = 0; i < nums.size() && k > 0; i++) {
-            if (nums[i] < 0) {
-                nums[i] = -nums[i];
-                k--;
-            }
-        }
 
-        // Step 3: If k is odd, flip the smallest element
-        if (k % 2 == 1) {
-            int mn = *min_element(nums.begin(), nums.end());
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] == mn) {
-                    nums[i] = -nums[i];
-                    break;
-                }
-            }
-        }
+while( i<n   && nums[i] < 0 && k> 0){
 
-        // Step 4: Calculate sum
-        int sum = 0;
-        for (int x : nums) {
-            sum += x;
+
+
+    nums[i] = -(nums[i]);
+
+i++;
+k--;
+
+}
+
+
+if(k%2 == 1){
+sort(nums.begin(), nums.end());
+nums[0] = -(nums[0]);
+
+}
+
+
+int sum =0;
+for ( int i : nums){
+sum += i;
         }
 
         return sum;
