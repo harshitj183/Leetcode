8b95b65d6e12d3a9447cdc3e3ee56cffc653b59d
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-01-15 11:43:17 +0530

    Time: 3 ms (14.27%) | Memory: 11 MB (45.56%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int minimumOperations(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        int count = 0;
-        int prev = 0;
-
-        for (int x : nums) {
-            if (x > prev) {
-                count++;
-                prev = x;
-            }
-        }
-        return count;
+   int minimumOperations(vector<int>& nums) {
+    unordered_set<int> s;
+    for (int x : nums) {
+        if (x > 0)
+            s.insert(x);
     }
+    return s.size();
+}
+
 };
