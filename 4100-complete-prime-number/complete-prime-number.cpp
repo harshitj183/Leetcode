9bdc9ce7c06cfea8ea97e1e85382b5561d5fd25b
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-04 19:59:01 +0530

    Time: 1 ms (43.98%) | Memory: 9.8 MB (16.25%) - LeetSync

---
+++ 4100-complete-prime-number/complete-prime-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * 1LL * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+
+    bool completePrime(int num) {
+        string s = to_string(num);
+        int n = s.size();
+
+        for (int k = 1; k <= n; k++) {
+            int prefix = stoi(s.substr(0, k));
+            int suffix = stoi(s.substr(n - k, k));
+
+            if (!isPrime(prefix) || !isPrime(suffix)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
