db5165d8e99f14aae0624adb31588b4f247bfd35
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:37 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/2/2026, 11:27:37 pm
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0;
+        for (int i = 0; i <= s.length(); ++i) {
+            if (i == s.length() || s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+        return s;
+    }
+};
