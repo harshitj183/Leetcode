af6029c013dbb5814d651aa523966a04f4c4376f
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:49 +0530

    Create 463-IslandPerimeter

---
+++ 463-IslandPerimeter/463-IslandPerimeter.cpp
@@ -0,0 +1,27 @@
+// Last updated: 5/2/2026, 11:27:49 pm
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+                  int perimeter = 0;
+        int rows = grid.size();
+              int cols = grid[0].size();
+        
+        for(int i = 0; i < rows; ++i) {
+          
+          
+            for(int j = 0; j < cols; ++j) {
+                                    if(grid[i][j] == 1) {
+                    perimeter += 4;  if(i > 0 && grid[i-1][j] == 1) {
+                                             perimeter -= 2;
+                    }
+                     
+                    if(j > 0 && grid[i][j-1] == 1) {
+                        perimeter -= 2;
+                    }
+                                     }
+                             }
+        }
+        
+        return perimeter;
+    }
+};
