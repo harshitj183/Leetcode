f8337d6f35c97f156c8e3d665ed49e4509366550
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:42 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/214-ShortestPalindrome.cpp
@@ -0,0 +1,32 @@
+// Last updated: 5/2/2026, 11:28:42 pm
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        string combined = s + "#" + rev;
+        int n = combined.size();
+
+        vector<int> lps(n, 0);
+
+        // Build LPS array
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];
+            while (j > 0 && combined[i] != combined[j]) {
+                j = lps[j - 1];
+            }
+            if (combined[i] == combined[j]) {
+                j++;
+            }
+            lps[i] = j;
+        }
+
+        // Characters to add in front
+        int palLen = lps[n - 1];
+        string toAdd = s.substr(palLen);
+        reverse(toAdd.begin(), toAdd.end());
+
+        return toAdd + s;
+    }
+};
