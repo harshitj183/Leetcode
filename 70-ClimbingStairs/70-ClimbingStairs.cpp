584e4d8d37d854764d287e6f9d6a41dbe3ecbbce
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:36 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/2/2026, 11:29:35 pm
+class Solution {
+public:
+    int climbStairs(int n) {
+                         if (n == 1) return 1;
+        int first = 1, second = 2;
+
+
+
+
+
+                  for (int i = 3; i <= n; i++) {
+            int temp = first + second;
+
+
+
+
+
+            first = second;
+                       second = temp;
+        }
+
+        return second;
+    }
+};
