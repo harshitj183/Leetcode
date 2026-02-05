28d771172bb8765ddfc7bcdc3bcb967e0dd76f50
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:07 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,18 @@
+// Last updated: 5/2/2026, 11:26:06 pm
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+       return "Hello World" ;
+    }
+};
+
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
