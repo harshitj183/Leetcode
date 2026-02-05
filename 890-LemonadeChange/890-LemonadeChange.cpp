c9ed4352866ba2a46dd39d7c3c0e0fc6f4a0f49e
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:09 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.cpp
@@ -0,0 +1,34 @@
+// Last updated: 5/2/2026, 11:27:09 pm
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+
+        int five = 0;
+        int ten = 0;
+
+        for (int i : bills) {
+
+            if (i == 5) {
+                five++;
+            } 
+            else if (i == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } 
+            else { 
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
