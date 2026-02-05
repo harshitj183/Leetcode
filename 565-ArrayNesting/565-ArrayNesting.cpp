cb50e8713979b4565eac88d6f1320ceb79216f67
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:33 +0530

    Create 565-ArrayNesting

---
+++ 565-ArrayNesting/565-ArrayNesting.cpp
@@ -0,0 +1,26 @@
+// Last updated: 5/2/2026, 11:27:33 pm
+ 
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> visited(n, false);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                int count = 0;
+                int curr = i;
+
+                while (!visited[curr]) {
+                    visited[curr] = true;
+                    curr = nums[curr];
+                    count++;
+                }
+
+                ans = max(ans, count);
+            }
+        }
+        return ans;
+    }
+};
