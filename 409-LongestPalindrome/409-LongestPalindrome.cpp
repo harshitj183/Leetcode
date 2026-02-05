2698512f6fde128587138761610c0b684aae8eeb
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:58 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.cpp
@@ -0,0 +1,25 @@
+// Last updated: 5/2/2026, 11:27:58 pm
+class Solution {
+public:
+    int longestPalindrome(string s) {
+                 unordered_map<char, int> count;
+                   for (char c : s) {
+            count[c]++;
+        }
+
+        int length = 0;
+                      bool hOdd = false;
+
+        for (auto& pair : count) {
+                           int freq = pair.second;
+            length += (freq / 2) * 2;  
+                       if (freq % 2 == 1) {
+                hOdd = true;   
+            }
+        }
+
+                   if (hOdd) length += 1;
+
+        return length;
+    }
+};
