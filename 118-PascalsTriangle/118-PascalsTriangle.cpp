32a5b2ded7021b5cfa2431f78a7d753d48d9ef0d
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:16 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/2/2026, 11:29:16 pm
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1); // har row ke sab elements initially 1
+
+            for (int j = 1; j < i; j++) {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle.push_back(row);
+        }
+
+        return triangle;
+    }
+};
