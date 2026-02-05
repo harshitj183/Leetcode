a607759bf6c79fa39260705ec790f0c218ae68c3
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:05 +0530

    Create 153-FindMinimumInRotatedSortedArray

---
+++ 153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/2/2026, 11:29:04 pm
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
