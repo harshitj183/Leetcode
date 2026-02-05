303ec8d5bc791f77dc1fd115e95f8a29dba8e66a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:14 +0530

    Create 343-IntegerBreak

---
+++ 343-IntegerBreak/343-IntegerBreak.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/2/2026, 11:28:13 pm
+class Solution {
+public:
+    int integerBreak(int n) {
+                            if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int product = 1;
+                  while (n > 4) {
+                        product = product * 3;
+                      n = n - 3;
+        }
+        product = product * n;
+  
+                       return product;
+    }
+};
