fab01b42ffdf2547df5d9f90b5168639037de51b
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:07 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
@@ -0,0 +1,9 @@
+// Last updated: 5/2/2026, 11:28:07 pm
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+      auto x = sqrt(num);  
+         int  temp1 = x; 
+        return x  == temp1;
+    }
+};
