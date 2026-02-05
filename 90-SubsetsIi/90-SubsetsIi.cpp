50be3f131d3b4ae8cce1fe8ec43f0a4ed500fa6d
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:24 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 5/2/2026, 11:29:24 pm
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        dfs(0, nums, temp, res);
+        return res;
+    }
+
+private:
+    void dfs(int idx, vector<int>& nums, vector<int>& temp, vector<vector<int>>& res) {
+        res.push_back(temp);
+        for (int i = idx; i < (int)nums.size(); ++i) {
+            // skip duplicates at the same recursion level
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            temp.push_back(nums[i]);
+            dfs(i + 1, nums, temp, res);
+            temp.pop_back();
+        }
+    }
+};
