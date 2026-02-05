7320744288c4f3ebe46ef18d3b6a5ff92ec50225
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:03 +0530

    Create 2860-SortBy

---
+++ 2860-SortBy/2860-SortBy.js
@@ -0,0 +1,9 @@
+// Last updated: 5/2/2026, 11:26:03 pm
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};
