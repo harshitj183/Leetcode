37333645679b2bdcec1bc37b73b19ab6ad333dfd
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:45 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,31 @@
+// Last updated: 5/2/2026, 11:28:45 pm
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+                         if (s.length() != t.length()) 
+        
+                      {return false;}
+
+        unordered_map<char, char> mapS;
+                     unordered_map<char, char> mapT;
+
+        for (int i = 0; i < s.length(); i++) {
+                    char chS = s[i];
+                           char chT = t[i];
+
+                           if (mapS.count(chS)) {
+                if (mapS[chS] != chT) return false;
+                         } else {
+                                   mapS[chS] = chT;
+            }
+
+                                      if (mapT.count(chT)) {
+                        if (mapT[chT] != chS) return false;
+            } else {
+                mapT[chT] = chS;
+            }
+        }
+
+        return true;
+    }
+};
