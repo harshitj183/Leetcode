72c5bbb8bacaeec2288335f15581a86daf504c98
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:25 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/2/2026, 11:28:25 pm
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
