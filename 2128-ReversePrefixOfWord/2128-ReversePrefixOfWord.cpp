e88e8cd7f6a7fe0722455289ae8901442953b6e7
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:27 +0530

    Create 2128-ReversePrefixOfWord

---
+++ 2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.cpp
@@ -0,0 +1,28 @@
+// Last updated: 5/2/2026, 11:26:26 pm
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int count = 0;
+        string str = "";
+        bool found = false;
+ 
+        for (char i : word) {
+            str += i;
+            if (i == ch) {
+                found = true;
+                break;
+            }
+            count++;
+        }
+ 
+        if (found) {
+            reverse(str.begin(), str.end());
+            for (int i = count + 1; i < word.size(); i++) {
+                str += word[i];
+            }
+            return str;
+        }
+
+        return word;
+    }
+};
