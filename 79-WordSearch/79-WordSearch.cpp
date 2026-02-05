827437601e760da8f3bd464de55efebfa25e717e
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:29 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.cpp
@@ -0,0 +1,39 @@
+// Last updated: 5/2/2026, 11:29:29 pm
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
+        // poora word mil gaya
+        if (idx == word.size()) return true;
+
+        // boundary + mismatch check
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()
+            || board[i][j] != word[idx])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#'; // visited mark
+
+        // 4 directions
+        bool found =
+            dfs(board, word, i+1, j, idx+1) ||
+            dfs(board, word, i-1, j, idx+1) ||
+            dfs(board, word, i, j+1, idx+1) ||
+            dfs(board, word, i, j-1, idx+1);
+
+        board[i][j] = temp; // backtrack
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
