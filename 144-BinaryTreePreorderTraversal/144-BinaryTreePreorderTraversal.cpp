80fa145f668b13d7fe371d9cc919076671d86e69
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:08 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,32 @@
+// Last updated: 5/2/2026, 11:29:07 pm
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+
+ 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preorder(root, result);
+        return result;
+        ;
+    }
+
+private:
+    void preorder(TreeNode* node, vector<int>& result) {
+        if (!node) return;
+        result.push_back(node->val);      // Visit root
+        preorder(node->left, result);     // Visit left
+        preorder(node->right, result);    // Visit right
+    }
+};
