e4e82ddbaa80a87daafe2b6729d16a7f39be3199
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:23 +0530

    Create 2416-EvaluateBooleanBinaryTree

---
+++ 2416-EvaluateBooleanBinaryTree/2416-EvaluateBooleanBinaryTree.cpp
@@ -0,0 +1,40 @@
+// Last updated: 5/2/2026, 11:26:22 pm
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        // Base: leaf node (0 or 1)
+        if (!root->left && !root->right) {
+            return root->val == 1;
+        }
+
+        // Recursively evaluate children
+        bool leftVal = evaluateTree(root->left);
+        bool rightVal = evaluateTree(root->right);
+
+        // Non-leaf: 2 = OR, 3 = AND
+        if (root->val == 2) {
+            return leftVal || rightVal;
+        } else { // root->val == 3
+            return leftVal && rightVal;
+        }
+    }
+};
+
