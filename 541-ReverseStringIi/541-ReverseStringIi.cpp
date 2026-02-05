8866e4c4740e96bb66a6b3d34d5fa782190f0df2
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:41 +0530

    Create 541-ReverseStringIi

---
+++ 541-ReverseStringIi/541-ReverseStringIi.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/2/2026, 11:27:40 pm
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.length(); i += 2 * k) {
+            if (i + k <= s.length()) {
+                reverse(s.begin() + i, s.begin() + i + k);
+            } else {
+                reverse(s.begin() + i, s.end());
+            }
+        }
+        return s;
+    }
+};
