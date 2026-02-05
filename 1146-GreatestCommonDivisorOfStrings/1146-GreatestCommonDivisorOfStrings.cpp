fd1fd37c4ff8c5385d4ff8dd42d2a86639c3a7c4
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:55 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,27 @@
+// Last updated: 5/2/2026, 11:26:55 pm
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+
+        string temp = "";
+ 
+                if (str1 + str2 != str2 + str1) {
+            return temp;   
+        }
+ 
+        int len1 = str1.size();
+          int len2 = str2.size();
+ 
+                   while (len2 != 0) {
+            int rem = len1 % len2;
+                        len1 = len2;
+            len2 = rem;
+        }
+ 
+        for (int i = 0; i < len1; i++) {
+                          temp = temp + str1[i];
+        }
+
+        return temp;
+    }
+};
