ebf6bf4f179f994bb32ecdd84b8e70f04eedc97a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:59 +0530

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/2/2026, 11:27:59 pm
+class Solution {
+public:
+    string toHex(int num) {
+      
+        if (num == 0) return "0";
+          string hexbhai ;
+        unsigned int n = num;    
+        const char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f'};
+ 
+        while (n > 0) {
+            hexbhai = hexDigits[n % 16] + hexbhai;   
+            n /= 16;  
+        }
+
+        return hexbhai; 
+    }
+};
