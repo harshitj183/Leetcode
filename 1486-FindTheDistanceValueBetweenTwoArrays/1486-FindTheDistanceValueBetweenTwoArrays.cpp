6ad139ca7356132370fee165854364b6d28a88e3
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:41 +0530

    Create 1486-FindTheDistanceValueBetweenTwoArrays

---
+++ 1486-FindTheDistanceValueBetweenTwoArrays/1486-FindTheDistanceValueBetweenTwoArrays.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/2/2026, 11:26:41 pm
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int distanceValue = 0;  
+        for (int x : arr1) {
+            bool isValid = true;  
+            for (int y : arr2) {
+                if (abs(x - y) <= d) {  
+                    isValid = false;  
+                    break;  
+                }
+            }
+             
+            if (isValid) {
+                distanceValue++;
+            }
+        }
+        
+        return distanceValue; 
+    }
+};
