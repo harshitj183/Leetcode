506e22a67a50e29594c7606242d90f6021a3adea
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:31 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cpp
@@ -0,0 +1,12 @@
+// Last updated: 5/2/2026, 11:26:30 pm
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int a = edges[0][0], b = edges[0][1];
+        int c = edges[1][0], d = edges[1][1];
+
+        // The repeated node in the first two edges is the center
+        if (a == c || a == d) return a;
+        return b;
+    }
+};
