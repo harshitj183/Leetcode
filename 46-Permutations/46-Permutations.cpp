1a009bb7108e7f3e7c715c56be0d72062aea209a
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:50 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,24 @@
+// Last updated: 5/2/2026, 11:29:50 pm
+class Solution {
+public:
+    void generatePermutations(vector<int>& nums, int start, vector<vector<int>>& result) {
+         
+        if (start == nums.size()) {
+                             result.push_back(nums);
+                                     return;
+        }
+        
+        
+        for (int i = start; i < nums.size(); ++i) {
+                  swap(nums[start], nums[i]);  
+            generatePermutations(nums, start + 1, result);  
+                        swap(nums[start], nums[i]);  
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+                                    vector<vector<int>> result;
+        generatePermutations(nums, 0, result);
+                                  return result;
+    }         
+};
