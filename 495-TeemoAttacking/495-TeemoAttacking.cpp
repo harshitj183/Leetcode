4beb5df1ba06a704f80c0f26eb2d377a22998ecd
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:47 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/2/2026, 11:27:46 pm
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n = timeSeries.size();
+        int total = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int gap = timeSeries[i + 1] - timeSeries[i];
+            total += min(duration, gap);
+        }
+
+        // last attack ka full duration add karo
+        total += duration;
+
+        return total;
+    }
+};
