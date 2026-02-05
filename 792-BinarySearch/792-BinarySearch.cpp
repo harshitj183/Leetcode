5c28f999b9e2030f709590b0fcdb8ef1a45a50fb
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:18 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,29 @@
+// Last updated: 5/2/2026, 11:27:17 pm
+class Solution {
+public:
+
+
+int  binarysearcch(vector<int>&nums,int target,int low,int high){
+
+ if(low>high)  return -1;
+int mid = ( low + high)/2;
+ if(nums[mid] == target) return  mid;
+  
+else if (nums[mid] < target ){
+return binarysearcch(nums,target,mid+1,high);
+}
+
+else{
+
+return binarysearcch( nums,target,low,mid-1);
+}
+
+}   int search(vector<int>& nums, int target) {
+      
+int low = 0;
+int high = nums.size()-1;
+      return binarysearcch(nums,target,low,high);
+ 
+
+    }
+};
