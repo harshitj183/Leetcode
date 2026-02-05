a928c3cfaad57d7057e2861984764ba44265d4ce
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:28:23 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.cpp
@@ -0,0 +1,29 @@
+// Last updated: 5/2/2026, 11:28:23 pm
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+                                                   unordered_map<char, string> charToWord;
+                               unordered_map<string, char> wordToChar;
+        istringstream iss(s);
+                                         vector<string> words;
+        string word;
+ 
+        while (iss >> word) words.push_back(word); 
+                        if (words.size() != pattern.size()) return false;
+
+                              for (int i = 0; i < pattern.size(); i++) {
+                                                 char c = pattern[i];
+            string w = words[i];
+
+             
+            if (charToWord.count(c) && charToWord[c] != w) return false;
+                            if (wordToChar.count(w) && wordToChar[w] != c) return false;
+
+             
+                          charToWord[c] = w;
+            wordToChar[w] = c;
+        }
+        
+        return true;
+    }
+};
