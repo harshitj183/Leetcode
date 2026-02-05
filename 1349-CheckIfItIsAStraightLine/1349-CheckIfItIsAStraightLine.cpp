03b3a943587866948ccb190727eada0d28f896e1
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:26:48 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.cpp
@@ -0,0 +1,24 @@
+// Last updated: 5/2/2026, 11:26:48 pm
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        
+        int n = coordinates.size();
+         
+        int x0 = coordinates[0][0], y0 = coordinates[0][1];
+                 int x1 = coordinates[1][0], y1 = coordinates[1][1];
+         
+        int dx = x1 - x0;
+                    int dy = y1 - y0;
+         
+        for (int i = 2; i < n; ++i) {
+                            int x = coordinates[i][0], y = coordinates[i][1];
+           
+            if ((y - y0) * dx != (x - x0) * dy) {
+                             return false;
+            }
+        }
+        
+        return true;
+    }
+};
