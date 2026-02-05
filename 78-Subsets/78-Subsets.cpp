4ca0a96c739ae3d90ae3f06109491e471e4193b1
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:29:30 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.cpp
@@ -0,0 +1,35 @@
+// Last updated: 5/2/2026, 11:29:30 pm
+class Solution {
+public:
+ 
+void solve(vector<int> nums,int idx,vector <int> temp,    vector<vector<int>> &res ){
+  res.push_back(temp);
+
+  
+for(int i= idx ;i< nums.size(); i++){
+
+ temp.push_back(nums[i]);
+
+    solve (nums,i+1,temp ,res );
+temp.pop_back();
+}
+  
+
+
+}
+
+ 
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+vector <int> temp;
+    vector<vector<int>>  res;
+
+solve(nums,0 ,temp,res);
+
+
+
+return res;
+    }
+};
