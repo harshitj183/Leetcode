8497c0174f2aec26bdf445185beef9556b880d51
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-02-05 23:27:44 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -0,0 +1,41 @@
+// Last updated: 5/2/2026, 11:27:44 pm
+class Solution {
+public:
+    
+ bool checkPerfectNumber(int num) {
+        
+        if (num == 1) {
+            return false;  
+        }
+
+        
+        int sumOfDivisors = 1;  
+ 
+        for (int i = 2; i <= sqrt(num); ++i) {  
+            if (num % i == 0) {  
+                sumOfDivisors += i;  
+                 
+                if (i != num / i) {
+                    sumOfDivisors += num / i;
+                }
+            }
+        }
+ 
+        if (sumOfDivisors == num) {
+            return true;  
+        } else {
+            return false; 
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+};
