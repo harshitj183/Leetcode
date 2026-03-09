347db1660dea961503183ccdc0f574021612460c
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-03-09 21:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (20.17%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     vector<int> countBits(int n) {
 
-    vector <int> ans;    
+    vector <int> ans;
+
+     
+        
           for( int i =0 ; i <=  n ;i++ ){
      
         bitset<32>  bits(i);
