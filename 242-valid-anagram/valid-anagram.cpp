5cb220365af5eb819052ffbac6bc9a01ca188205
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-04-02 12:23:14 +0530

    Time: 8 ms (7.35%) | Memory: 9.6 MB (73.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,63 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       if (s.size() != t.size())   return false;
-map <char ,int >  res;
-map <char,int > res1;
-for (auto i : s)  res[i]++;
-for (auto k : t)   res1[k]++;
- for (int i =  0; i <res.size();i++){
-
-
-if (res[i] != res1[i]) return false;
- }
-
-
-
-return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});
