459eaa88027e5cc185a1f11b213b57faebe1f7e6
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-04-04 00:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.59%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n;   // handle INT_MIN safely
-        
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
 
-        return fastPow(x, N);
-    }
 
-private:
-    double fastPow(double x, long long n) {
-        if (n == 0) return 1.0;
 
-        double half = fastPow(x, n / 2);
 
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+    return pow(x,n) ;  
     }
 };
