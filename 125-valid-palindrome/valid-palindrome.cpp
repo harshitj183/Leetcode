628bbe8e231c6bdba07dacc6825dec553dc54412
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-04-08 11:12:31 +0530

    Time: 5 ms (7.91%) | Memory: 10 MB (36.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-    int   slen = s.length() ;
+        string s2 = "";
 
-string snew;
-   char temp; 
-    for (int i =0;i<slen;i++){
+        for (char i : s) {
 
-if (isalpha(s[i])){
+            if (isalnum(i)) {
 
- temp  = tolower(s[i]);
-
-
-    snew.push_back(temp);
-
-
-
-    }
- 
-if (isdigit(s[i])){
-
-  
-
-temp = s[i];
-    snew.push_back(temp);
- }
- 
-
-
-}
-
-
-
-bool isPalindrome = true;
-
-    int n = snew.length();
-    for (int i = 0; i < n / 2; i++) {
-        if (snew[i] != snew[n - 1 - i]) {
-            isPalindrome = false;
-            break;
+                s2 += tolower(i);
+            }
         }
-    }
+
+        if (s2.size() == 1) {
+
+            return true;
+        }
+        if (s2.size() % 2 == 0) {
+
+            for (int i = 0; i < s2.size(); i++) {
+
+                if (s2[i] != s2[s2.size() - 1 - i]) {
+
+                    return false;
+                }
+            }
+        }
+
+        else {
 
 
+        for (int i =0 ; i < s2.size()/2;i++){
 
-return isPalindrome;
 
+            if ( s2[i] != s2[s2.size() -1-i]  ) return false;
+        }
+        }
+
+        return true;
     }
 };
