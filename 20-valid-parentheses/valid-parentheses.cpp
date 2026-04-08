0172df74f5e476902423a742108c922cd270c126
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-04-08 12:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (88.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<char> st;
 
-        if (s.size() % 2 != 0) return false;
-
-        string temp = "";   // 🔥 temp ko memory bana diya
-
-        for (int i = 0; i < s.size(); i++) {
-
-            char ch = s[i];
-
-            // opening brackets → temp me add
-            if (ch == '(' || ch == '{' || ch == '[') {
-                temp.push_back(ch);
-            }
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } 
             else {
-                // closing bracket but no opening remembered
-                if (temp.size() == 0) return false;
+                if (st.empty()) return false;
 
-                char last = temp.back();   // last open
-                temp.pop_back();
+                char top = st.top();
+                st.pop();
 
-                // mismatch check (tumhari hi logic)
-                if ((last == '(' && ch != ')') ||
-                    (last == '{' && ch != '}') ||
-                    (last == '[' && ch != ']')) {
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
                     return false;
                 }
             }
         }
 
-        // agar sab close ho gaye
-        return temp.size() == 0;
+        return st.empty();
     }
 };
