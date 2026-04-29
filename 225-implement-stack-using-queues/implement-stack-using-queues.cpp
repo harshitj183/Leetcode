c4934d5740e6db3917665570e8276720c1acc621
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-04-29 23:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (80.64%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -4,29 +4,48 @@ public:
     queue<int> q1, q2;
 
     void push(int x) {
-        q2.push(x);
+       
+      q2.push(x);
 
-        // move all elements from q1 → q2
-        while (!q1.empty()) {
-            q2.push(q1.front());
-            q1.pop();
-        }
 
-        // swap q1 and q2
-        swap(q1, q2);
+
+while(!q1.empty()){
+   q2.push(q1.front());
+
+q1.pop();
+
+
+}
+
+
+
+
+
+    swap(q1,q2);
+
+
+
     }
 
     int pop() {
-        int val = q1.front();
-        q1.pop();
-        return val;
+        
+   int temp = q1.front();
+
+  q1.pop();
+        return   temp;
+
+
+
     }
 
     int top() {
-        return q1.front();
+       return  q1.front();
+       
     }
 
     bool empty() {
-        return q1.empty();
+
+       return  q1.empty();
+       
     }
 };
