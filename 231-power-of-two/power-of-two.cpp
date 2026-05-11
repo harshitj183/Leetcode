3ce337f2bd17b4957f822cdd5a1fc91ed3353206
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-11 11:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (11.59%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,13 @@
+ 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
 
- if (n >= 1 && !(n & (n-1))){
+ if (n >= 1 && !(n & (n-1))) 
 
     return true;
- }
+ 
 return false;
 
     }
