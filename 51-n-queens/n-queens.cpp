e0da69271f2af45e62f0d949ca6e01934267612f
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-14 11:05:02 +0530

    Time: 5 ms (21.55%) | Memory: 10 MB (93.27%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
-
-
-
- void backtrack(int row, vector<string> &board, vector<vector<string>> &res) {
-        if (row == board.size()) {
-            res.push_back(board);
-            return;
-        }
-
-        for (int c = 0; c < board.size(); c++) {
-            if (issafe(row, c, board)) {
-                board[row][c] = 'Q';
-                backtrack(row + 1, board, res);
-                board[row][c] = '.';
-            }
-        }
-    } 
-
-    bool issafe(int r, int c, vector<string> &board) {
+bool issafe(int r, int c, vector<string> &board) {
     int n = board.size();
 
     // check column (up)
@@ -39,12 +21,26 @@ public:
     return true;
 }
  
-   vector<vector<string>> solveNQueens(int n) {
 
 
+ void backtrack(int row, vector<string> &board, vector<vector<string>> &res) {
+        if (row == board.size()) {
+            res.push_back(board);
+            return;
+        }
 
+        for (int c = 0; c < board.size(); c++) {
+            if (issafe(row, c, board)) {
+                board[row][c] = 'Q';
+                backtrack(row + 1, board, res);
+                board[row][c] = '.';
+            }
+        }
+    } 
 
-
+    vector<vector<string>> solveNQueens(int n) {
+  
+  
 
  vector<vector<string>> res;
         vector<string> board(n, string(n, '.'));
