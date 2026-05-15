a1c3bd47c58c1d1861a6bfbf9f32aad117c434a8
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-15 14:57:51 +0530

    Time: 4 ms (33.91%) | Memory: 9.8 MB (48.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+   
 
-        return s == t;
+
+   if ( s.length() != t.length()){
+
+    return false;
+   }
+
+unordered_map<int ,int > map;
+
+        for(int i =0; i< s.size();i++){
+
+           
+map[s[i]]++;
+map[t[i]]--;
+
+ 
+        }
+
+for (auto var : map){
+
+
+    if (var.second){
+
+        return false;
+    }
+}
+
+ return true;       
     }
 };
