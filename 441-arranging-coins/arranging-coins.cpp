d23c1a0e13a26f5c923ab195665d1c9cca0e9441
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-20 17:55:33 +0530

    Time: 3 ms (34.18%) | Memory: 8.8 MB (33.97%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,13 +1,22 @@
-#include <cmath>
 class Solution {
 public:
     int arrangeCoins(int n) {
-        
-        long long N = static_cast<long long>(n);
-        
-      
-        long long l = (-1 + sqrt(1 + 8 * N)) / 2;
-        
-         return static_cast<int>(l);
+        long long left = 1, right = n;
+        long long ans = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+
+            long long coins = mid * (mid + 1) / 2;
+
+            if (coins <= n) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
     }
 };
