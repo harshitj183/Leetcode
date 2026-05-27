1a0492d29b24d6fbb26995e0d2c2855f14097574
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-27 11:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (75.11%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,37 +1,53 @@
-
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int orig, int color) {
-        int m = image.size(), n = image[0].size();
+
+void solve(vector<vector<int>>& image,
+           int sr,
+           int sc,
+           int oldColor,
+           int color){
  
-
-        // Out of bounds
-        if (r < 0 || r >= m || c < 0 || c >= n)
-            return;
-
-        // Not the target color
-        if (image[r][c] != orig)
-            return;
-
-        // Fill current cell
-        image[r][c] = color;
-
-        // Explore 4-directionally
-        dfs(image, r + 1, c, orig, color);
-        dfs(image, r - 1, c, orig, color);
-        dfs(image, r, c + 1, orig, color);
-        dfs(image, r, c - 1, orig, color);
+    if (sr < 0){
+        return;
     }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int orig = image[sr][sc];
+    if (sc < 0){
+        return;
+    }
 
-        // If original color is same as new color, no need to fill
-        if (orig == color)
-            return image;
+    if (sr > image.size()-1){
+        return;
+    }
 
-        dfs(image, sr, sc, orig, color);
+    if (sc > image[0].size()-1){
+        return;
+    }
+ 
+    if (image[sr][sc] != oldColor){
+        return;
+    }
+ 
+    image[sr][sc] = color;
+ 
+    solve(image,sr+1,sc,oldColor,color);
+    solve(image,sr,sc+1,oldColor,color);
+    solve(image,sr-1,sc,oldColor,color);
+    solve(image,sr,sc-1,oldColor,color);
+}
+
+vector<vector<int>> floodFill(vector<vector<int>>& image,
+                              int sr,
+                              int sc,
+                              int color) {
+
+    int oldColor = image[sr][sc];
+ 
+    if (oldColor == color){
         return image;
     }
+
+    solve(image,sr,sc,oldColor,color);
+
+    return image;
+}
 };
- 
