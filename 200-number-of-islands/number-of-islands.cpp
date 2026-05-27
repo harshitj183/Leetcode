b0e2973319c8f03a73814f22eda56ed673f6a061
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-05-27 13:07:28 +0530

    Time: 25 ms (72.51%) | Memory: 15.9 MB (100.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,17 +3,15 @@ public:
 
     void dfs(vector<vector<char>>& grid, int row, int col) {
 
-        // boundary + invalid checks
+     
         if (row < 0 || col < 0 ||
             row >= grid.size() || col >= grid[0].size() ||
             grid[row][col] != '1') {
             return;
         }
-
-        // mark visited
+ 
         grid[row][col] = '9';
 
-        // 4 directions
         dfs(grid, row - 1, col);
         dfs(grid, row + 1, col);
         dfs(grid, row, col - 1);
