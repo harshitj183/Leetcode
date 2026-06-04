621b72434d4a144e9d64c1f7268757aecd79bd39
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-06-04 21:40:42 +0530

    Time: 6 ms (12.66%) | Memory: 15.7 MB (23.37%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -3,16 +3,23 @@ public:
     string destCity(vector<vector<string>>& paths) {
         unordered_set<string> source;
 
-        for (auto &p : paths) {
-            source.insert(p[0]);
-        }
+       
 
-        for (auto &p : paths) {
-            if (source.find(p[1]) == source.end()) {
-                return p[1];
-            }
-        }
 
-        return "";
+
+       for(auto i : paths){
+
+
+        source.insert(i[0]);
+       }
+
+for(auto &p :paths ){
+if(source.find(p[1]) ==  source.end() ){
+
+    return p[1];
+}
+}
+
+       return "";
     }
 };
