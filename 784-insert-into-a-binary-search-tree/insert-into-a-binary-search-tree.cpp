a6e5ba0a933106f29eec42379d15fc08b000b61e
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-06-21 09:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (57.65%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -12,19 +12,28 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-            if(root == nullptr)  {
+          if(root==nullptr){
 
-            return  new TreeNode(val);
-            }
+  return  new TreeNode(val);
 
-            if ((root->val) < val) {
-                root->right =  insertIntoBST(root->right, val);
-            }
-               if (root->val > val){
-                root->left = insertIntoBST(root->left, val);
-            }
+          }
 
- 
- return root;
+if((root->val) < val){
+
+
+root-> right=  insertIntoBST(root->right,val);
+}
+
+if( (root->val)  > val  ){
+
+
+    root->left = insertIntoBST(root->left,val);
+}
+
+
+
+
+
+return root;
     }
 };
