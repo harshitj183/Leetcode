61f60165ab33d53fa5a8bcf9eb1a5af9d5cc5c54
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-07-04 13:17:40 +0530

    Time: 3 ms (61.49%) | Memory: 93.8 MB (88.58%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        
-vector<int>visited(n,0);
 
-for(int i =0 ; i < edges.size(); i++){
+        vector<int> visited(n, 0);
 
+        for (int i = 0; i < edges.size(); i++) {
 
-visited[edges[i][1]]++;
+            visited[edges[i][1]]++;
+        }
 
-}
+        int count = 0;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
 
-int count =0;
-int res = -1;
-for(int i  =0  ; i < n;i++){
+            if (!visited[i]) {
+                res = i;
+                count++;
+            }
 
+            if (count > 1) {
 
-    if(!visited[i]){
-  res =   i;
-   count++;
-    }
+                return -1;
+            }
+        }
 
-
-    if(count>1){
-
-
-        return -1;
-    }
-}
-
-
-
- return res;
+        return res;
     }
 };
