1d80c5cc98b301eeaa9abeb28b30172121f25276
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-07-04 12:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 99.1 MB (67.90%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int a = edges[0][0], b = edges[0][1];
-        int c = edges[1][0], d = edges[1][1];
 
-        // The repeated node in the first two edges is the center
-        if (a == c || a == d) return a;
-        return b;
+
+
+
+      if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
+
+
+      return  edges[0][0] ;
+      } else{
+
+      return  edges[0][1];
+      }
+
+
+      return -1; 
     }
 };
