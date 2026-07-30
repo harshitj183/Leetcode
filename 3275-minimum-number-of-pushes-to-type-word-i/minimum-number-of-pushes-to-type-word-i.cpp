6d3e6c42e9c1e4d877ac54b35031ee9d77e59274
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-07-30 22:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.73%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -6,19 +6,17 @@ public:
 
         int res = 0;
 
-        // Complete groups of 8
+       
         int n = sz / 8;
-
-        // Remaining letters
+ 
         int rem = sz % 8;
 
-        // Cost of complete groups
-        // 8*1 + 8*2 + 8*3 + ...
+     
         int temp = 8 * (n * (n + 1) / 2);
 
         res += temp;
 
-        // Cost of remaining letters
+      
         res += rem * (n + 1);
 
         return res;
