cfb70fbbe825222ec9ca41ea555d546b38bb923e
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-08-08 12:39:36 +0530

    Time: 71 ms (6.52%) | Memory: 55.4 MB (29.17%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -3,18 +3,13 @@
  * @return {Function}
  */
 var compose = function(functions) {
-	if (functions.length === 0) {
-    return function(x) { return x; };
-  }
 
-  return functions.reduceRight(function(prevFn, nextFn) {
     return function(x) {
-      return nextFn(prevFn(x));
+
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+
+        return x;
     };
-  });
-
 };
-
-
-const fn = compose([x => x + 1, x => 2 * x]);
-console.log(fn(4)); // 9
