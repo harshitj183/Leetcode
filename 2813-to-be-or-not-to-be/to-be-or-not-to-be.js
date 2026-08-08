8d6462d43673d526ec69876ec787c9f00b97e246
Harshit Jaiswal <76927137+harshitj183@users.noreply.github.com>
2026-08-08 11:48:13 +0530

    Time: 39 ms (81.97%) | Memory: 54.1 MB (17.39%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+var expect = function(val) {
+
+    return {
+         
+
+        toBe: function(value) {
+            if (value ===  val)
+                return true;
+            else
+                throw new Error("Not Equal");
+        },
+
+        notToBe: function(value) {
+            if (value !==  val)
+                return true;
+            else
+                throw new Error("Equal");
+        }
+    };
+};
